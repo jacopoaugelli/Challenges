e4c7d890bf2f1d4ab6f95bc8631b5c0c501dde42
Jacopo Augelli <106108757+jacopoaugelli@users.noreply.github.com>
2023-07-28 16:55:12 +0200

    Update 04_Fu.py

--- PortSwigger/File Upload/04_Fu.py
+++ PortSwigger/File Upload/04_Fu.py
@@ -2,7 +2,7 @@ import requests
 import argparse
 import re
 
-parser = argparse.ArgumentParser(description='Solves the following Lab: Web shell upload via path traversal')
+parser = argparse.ArgumentParser(description='Solves the following Lab: Web shell upload via extension blacklist bypass')
 parser.add_argument('--id', action='store', help='lab ID, ex: 0a6e00ec03ca2e848083672100ee00fb', required=True)
 lab = parser.parse_args()
 
